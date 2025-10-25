ff50b6dbe324c0fc675f1c24b3662207f1c05435
dventasfdez <dventasfdez@gmail.com>
2025-10-25 11:09:20 +0200

    feat: Working on migration to Vite

---
+++ src/components/button/button.stories.tsx
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Button from "./button";
+
+const meta: Meta<typeof Button> = {
+  title: "Components/Button",
+  component: Button,
+  tags: ["autodocs"],
+  args: {
+    children: "Button",
+  },
+  argTypes: {
+    onClick: { action: "clicked" },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const Primary: Story = {
+  args: { primary: true },
+};
+
+export const Secondary: Story = {
+  args: { secondary: true },
+};
+
+export const CTA: Story = {
+  args: { cta: true },
+};
+
+export const WithIconLeft: Story = {
+  args: { icon: { value: "check", position: "left" } },
+};
+
+export const WithIconRight: Story = {
+  args: { icon: { value: "check", position: "right" } },
+};
