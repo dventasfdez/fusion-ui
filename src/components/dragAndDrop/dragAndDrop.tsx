fbd543a0f9ca3ce59dbf1293bcdfb0a4f8a4cc90
dventasfdez <dventasfdez@gmail.com>
2025-11-26 11:56:53 +0100

    wip: file input

--- src/components/dragAndDrop/dragAndDrop.tsx
+++ src/components/dragAndDrop/dragAndDrop.tsx
@@ -1,4 +1,4 @@
-import React, {useRef, useEffect} from 'react';
+import React, { useRef, useEffect } from "react";
 
 interface IDragAndDropProps {
   disabled?: boolean;
@@ -9,7 +9,7 @@ interface IDragAndDropProps {
 }
 
 const DragAndDrop: React.FC<IDragAndDropProps> = (props) => {
-  const {disabled, children, className, onClick, ...rest} = props;
+  const { disabled, children, className, onClick, ...rest } = props;
   const dropRef = useRef<HTMLDivElement>(null);
 
   const handleDrag = (e: any) => {
@@ -30,11 +30,16 @@ const DragAndDrop: React.FC<IDragAndDropProps> = (props) => {
   const handleDrop = (e: any) => {
     e.preventDefault();
     e.stopPropagation();
-    if (!disabled && e?.dataTransfer?.files && e.dataTransfer.files.length > 0) {
-      if (typeof props.handleDrop === 'function') props.handleDrop(e.dataTransfer.files);
+    if (
+      !disabled &&
+      e?.dataTransfer?.files &&
+      e.dataTransfer.files.length > 0
+    ) {
+      if (typeof props.handleDrop === "function")
+        props.handleDrop(e.dataTransfer.files);
       if (e?.dataTransfer?.items) {
         e.dataTransfer.items.clear();
-      } else if (typeof e?.dataTransfer?.clearData === 'function') {
+      } else if (typeof e?.dataTransfer?.clearData === "function") {
         e.dataTransfer.clearData();
       }
     }
@@ -43,27 +48,33 @@ const DragAndDrop: React.FC<IDragAndDropProps> = (props) => {
   useEffect(() => {
     const divRender = dropRef.current;
     if (!divRender) return;
-    divRender.addEventListener('dragenter', handleDragIn);
-    divRender.addEventListener('dragleave', handleDragOut);
-    divRender.addEventListener('dragover', handleDrag);
-    divRender.addEventListener('drop', handleDrop);
+    divRender.addEventListener("dragenter", handleDragIn);
+    divRender.addEventListener("dragleave", handleDragOut);
+    divRender.addEventListener("dragover", handleDrag);
+    divRender.addEventListener("drop", handleDrop);
     return () => {
       if (!divRender) return;
-      divRender.removeEventListener('dragenter', handleDragIn);
-      divRender.removeEventListener('dragleave', handleDragOut);
-      divRender.removeEventListener('dragover', handleDrag);
-      divRender.removeEventListener('drop', handleDrop);
+      divRender.removeEventListener("dragenter", handleDragIn);
+      divRender.removeEventListener("dragleave", handleDragOut);
+      divRender.removeEventListener("dragover", handleDrag);
+      divRender.removeEventListener("drop", handleDrop);
     };
   });
 
   return (
     <div
-      data-testid={rest['data-testid'] ? rest['data-testid'] : undefined}
+      data-testid={rest["data-testid"] ? rest["data-testid"] : undefined}
       className={`drag-drop ${className}`}
-      style={{display: 'inline-block', position: 'relative', width: '100%'}}
+      style={{
+        display: "inline-flex",
+        position: "relative",
+        width: "100%",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
       ref={dropRef}
       onClick={() => {
-        if (!disabled && typeof onClick === 'function') onClick();
+        if (!disabled && typeof onClick === "function") onClick();
       }}
     >
       {children}
